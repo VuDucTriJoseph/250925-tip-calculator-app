6db70a2a2e4d98e41bbe1d575e732004e2db4adc
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-09-26 12:04:52 +0700

    done with logic

---
+++ main.js
@@ -0,0 +1,35 @@
+const billInfo = document.getElementById("bill-info")
+const tipPerPeople = document.getElementById("tip-per-people");
+const totalPerPeople = document.getElementById("total-per-people");
+const resetBtn = document.getElementById("btn-reset");
+
+// console.log(billInfo);
+function calBill(bill,tip=0,numOfPeople){
+    const tipAmount = bill * tip/100;
+    const billAmount =  Number(bill) + tipAmount;
+    
+    // console.log('billamount: ', billAmount);
+    return [tipAmount / numOfPeople, billAmount/numOfPeople];
+}
+
+// 
+billInfo.addEventListener('change',e=>{
+    e.preventDefault();
+    const formData =Object.fromEntries(new FormData(billInfo));
+    console.log(formData);
+    if (!formData.numOfPeople || !formData.tip || !formData.numOfPeople  ) return ; // avoid 
+    
+    const result = calBill(formData.bill,formData.tip,formData.numOfPeople)
+    console.log(result)
+    tipPerPeople.innerText = `$ ${result[0].toFixed(2)}`;
+    totalPerPeople.innerText = `$ ${result[1].toFixed(2)}`;
+});
+
+// reset
+
+resetBtn.addEventListener('click',e=>{
+    billInfo.reset();
+    tipPerPeople.innerText = "$";
+    totalPerPeople.innerText = "$";
+})
+
